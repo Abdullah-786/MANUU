9bfe375e462ad4057f62dd0fc6450feae5629f3c
Abdullah-786 <56754013+Abdullah-786@users.noreply.github.com>
2019-10-23 23:41:44 +0530

    Rename README.AR to 3_Sum of digit.py

---
+++ 3_Sum of digit.py
@@ -0,0 +1,10 @@
+# MANUU
+ASSIGNMENT
+x = int(input("Enter the number for finding a sum"))
+num = x
+sum1 =0
+while(a!=0)
+m = x%10
+sum1+=n
+x = x//10
+printf("sum of the digit of a number is",sum1,num)
